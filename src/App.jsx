c620b1d2c60ba1010b7375d3ad01249119ba1b09
Rafiu-14 <rafiumd98@gmail.com>
2024-06-22 14:21:53 +0600

    Intialized file and fixing some error

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl bg-slate-700 text-white font-bold p-2 uppercase'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
